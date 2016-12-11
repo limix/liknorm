60be79ba4023372039ff9eb5839501ffe0e1ec39
Danilo Horta <danilo.horta@gmail.com>
2016-12-11 16:00:18 +0000

    machine

--- liknorm/machine.h
+++ liknorm/machine.h
@@ -1,16 +1,21 @@
 #ifndef MACHINE_H
 #define MACHINE_H
 
+struct Normal;
+struct ExpFam;
+
 struct LikNormMachine
 {
   double *log_zeroth; // log(mom0)
   double *u; // mom1/mom0
   double *v; // mom2/mom0
-  int     size;
-  double *A0;
-  double *logA1;
-  double *logA2;
-  double *diff;
+  double *A0; // array for the log partition function
+  double *logA1; // array for log(A'(x))
+  double *logA2; // array for log(A''(x))
+  double *diff; // temporary array
+  int     size; // size of the above arrays
+  struct ExpFam ef;
+  struct Normal normal;
 };
 
 #endif
