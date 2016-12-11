e25305e99da6f18043cab07dd49fbed8d3cf794d
Danilo Horta <danilo.horta@gmail.com>
2016-12-11 02:26:37 +0000

    first compilation

--- liknorm/machine.h
+++ liknorm/machine.h
@@ -1,16 +1,16 @@
 #ifndef MACHINE_H
 #define MACHINE_H
 
-typedef struct
+struct LikNormMachine
 {
   double *log_zeroth; // log(mom0)
   double *u; // mom1/mom0
   double *v; // mom2/mom0
-  int     n;
+  int     size;
   double *A0;
   double *logA1;
   double *logA2;
   double *diff;
-} LikNormMachine;
+};
 
 #endif
