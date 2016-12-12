5cfbee7abc2872857c650e5a9616e0e34538772d
Danilo Horta <danilo.horta@gmail.com>
2016-12-12 16:55:58 +0000

    revmoed comment

--- liknorm/liknorm.h
+++ liknorm/liknorm.h
@@ -11,11 +11,6 @@ void liknorm_integrate(LikNormMachine *machine,
 void liknorm_destroy_machine(LikNormMachine *machine);
 
 void liknorm_set_bernoulli(LikNormMachine *machine, double k);
-
-/* Binomial likelihood.
-   k: number of successes
-   n: number of trials
-*/
 void liknorm_set_binomial(LikNormMachine *machine, double k, double n);
 void liknorm_set_poisson(LikNormMachine *machine, double k);
 void liknorm_set_exponential(LikNormMachine *machine, double x);
