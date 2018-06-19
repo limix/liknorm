5e260a9f16bc3e2ebabfe76498b912fb54613255
Danilo Horta <danilo.horta@gmail.com>
2018-06-19 13:02:41 +0100

    remove support for old mvc

--- src/compiler.h
+++ src/compiler.h
@@ -14,23 +14,4 @@
 #endif
 #endif
 
-#if defined(_MSC_VER)
-#if _MSC_VER <= 1500
-#include "hcephes.h"
-#include <float.h>
-#define log1p hcephes_log1p
-inline double fmax(double left, double right) {
-    return (left > right) ? left : right;
-}
-
-inline double fmin(double left, double right) {
-    return (left < right) ? left : right;
-}
-
-#define lgamma hcephes_lgam
-#define isnan(x) _isnan(x)
-#define isfinite(x) _finite(x)
-#endif
-#endif
-
 #endif
