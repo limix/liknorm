05b5d0c2868b5465f08e5421bc83e8e175197fbd
Danilo Horta <danilo.horta@gmail.com>
2018-07-05 14:05:34 +0100

    byby

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
 @echo off
 SETLOCAL
+pushd .
+cd %Temp%
 
 :: Log file
 SET ORIGIN=%cd%
@@ -11,7 +13,8 @@ SETLOCAL ENABLEEXTENSIONS
 IF ERRORLEVEL 1 ECHO Unable to enable extensions
 
 :: Configuration
-set VERSION_URL=https://raw.githubusercontent.com/limix/liknorm/master/VERSION
+set BRANCH=develop
+set VERSION_URL=https://raw.githubusercontent.com/limix/liknorm/%BRANCH%/VERSION
 call :winget "%VERSION_URL%" >>%LOG_FILE% 2>&1
 set /p VERSION=<VERSION && del VERSION
 set FILE=liknorm-%VERSION%.zip
@@ -22,7 +25,6 @@ if DEFINED ARCH set ARCH=%ARCH:"=%
 if NOT DEFINED ARCH (set ARCH=x64) else if [%ARCH%]==[x86] (set ARCH=)
 set "CFLAGS=/MD /GL"
 
-exit /B 0
 echo [0/4] Library(liknorm==%VERSION%)
 
 :: Cleaning up previous mess
@@ -34,25 +36,25 @@ copy /y nul %LOG_FILE% >nul 2>&1
 echo|set /p="[1/4] Downloading... "
 echo Fetching %URL% >>%LOG_FILE% 2>&1
 call :winget "%URL%" >>%LOG_FILE% 2>&1
-if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
+if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && call :byebye 1) else (echo done.)
 
 echo|set /p="[2/4] Extracting... "
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%FILE%', '.'); }"
-if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
+if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && call :byebye 1) else (echo done.)
 
 cd %DIR% && mkdir build && cd build
 
 echo|set /p="[3/4] Configuring... "
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR_PLATFORM=%ARCH% -DCMAKE_INSTALL_PREFIX="%programfiles%\liknorm" >>%LOG_FILE% 2>&1
-if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
+if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && call :byebye 1) else (echo done.)
 
 echo|set /p="[4/4] Compiling and installing... "
 cmake --build . --config Release --target install >>%LOG_FILE% 2>&1
-if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1)
-if NOT exist %programfiles%\liknorm\lib\liknorm.lib (echo FAILED. && type %LOG_FILE% && exit /B 1)
-if NOT exist %programfiles%\liknorm\bin\liknorm.dll (echo FAILED. && type %LOG_FILE% && exit /B 1)
-if NOT exist %programfiles%\liknorm\lib\liknorm_static.lib (echo FAILED. && type %LOG_FILE% && exit /B 1)
-if NOT exist %programfiles%\liknorm\include\liknorm.h (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
+if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && call :byebye 1)
+if NOT exist %programfiles%\liknorm\lib\liknorm.lib (echo FAILED. && type %LOG_FILE% && call :byebye 1)
+if NOT exist %programfiles%\liknorm\bin\liknorm.dll (echo FAILED. && type %LOG_FILE% && call :byebye 1)
+if NOT exist %programfiles%\liknorm\lib\liknorm_static.lib (echo FAILED. && type %LOG_FILE% && call :byebye 1)
+if NOT exist %programfiles%\liknorm\include\liknorm.h (echo FAILED. && type %LOG_FILE% && call :byebye 1) else (echo done.)
 
 cd %ORIGIN% >nul 2>&1
 del /q %FILE% >nul 2>&1
@@ -68,6 +70,11 @@ set Path1=%~1
 set Path2=%~2
 if {%Path1:~-1,1%}=={\} (set Result=%Path1%%Path2%) else (set Result=%Path1%\%Path2%)
 goto :eof
+
+:byebye
+popd
+exit /B %~1
+goto :eof 
 
 :winget    - download file given url
 set URL=%~1
