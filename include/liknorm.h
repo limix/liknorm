c3ddff9d66ce53d6697dcf7149635e122751adf0
Danilo Horta <danilo.horta@gmail.com>
2019-02-04 19:17:08 +0000

    travis ci

--- include/liknorm.h
+++ include/liknorm.h
@@ -2,13 +2,13 @@
 #define LIKNORM_H
 
 /** Major bgen version. */
-#define LIKNORM_VERSION_MAJOR 3
+#define LIKNORM_VERSION_MAJOR 1
 /** Minor bgen version. */
-#define LIKNORM_VERSION_MINOR 0
+#define LIKNORM_VERSION_MINOR 5
 /** Minor bgen version. */
-#define LIKNORM_VERSION_PATCH 2
+#define LIKNORM_VERSION_PATCH 0
 /** Bgen version. */
-#define LIKNORM_VERSION "3.0.2"
+#define LIKNORM_VERSION "1.5.0"
 
 #ifdef __cplusplus
 extern "C"
